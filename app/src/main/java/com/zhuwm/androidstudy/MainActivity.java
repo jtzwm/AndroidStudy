12a9bac9f04b2de735e07c4d0fd90df84a5a9c27
zhuwm <zhuwm@cairenhui.com>
2016-03-24 14:25:51 +0800

    commit by zhuwm

--- app/src/main/java/com/zhuwm/androidstudy/MainActivity.java
+++ app/src/main/java/com/zhuwm/androidstudy/MainActivity.java
@@ -1,13 +1,24 @@
 package com.zhuwm.androidstudy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnNext).setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,WebViewActivity.class));
+
+            }
+        });
     }
 }
