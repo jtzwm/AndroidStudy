40f5f43a08c9446eaec1da5cbb7e35ddee40b08a
zhuwm <zhuwm@cairenhui.com>
2016-03-27 19:31:32 +0800

    commit

--- app/src/main/java/com/zhuwm/androidstudy/WebViewActivity.java
+++ app/src/main/java/com/zhuwm/androidstudy/WebViewActivity.java
@@ -2,12 +2,43 @@ package com.zhuwm.androidstudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        webView= (WebView) findViewById(R.id.webView);
+        webView.loadUrl("http://localhost:8080/h5test/index.do");
+
+        webView.setWebViewClient(new MyWebViewClinet());
+
+    }
+
+
+    @Override
+    //设置回退
+    //覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack(); //goBack()表示返回WebView的上一页面
+            return true;
+        }
+        return false;
+    }
+
+    private class MyWebViewClinet extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
